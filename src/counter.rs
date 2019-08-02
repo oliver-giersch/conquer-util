ec57debcc23c8232524142a641cf5ff7912f0887
Oliver Giersch <oliver.giersch@b-tu.de>
2019-08-02 10:32:54 +0200

    improves ThreadCounter

--- src/counter.rs
+++ src/counter.rs
@@ -11,36 +11,84 @@ pub struct ThreadCounter {
     counters: Box<[Count]>,
 }
 
+/********** impl inherent *************************************************************************/
+
 impl ThreadCounter {
     #[inline]
     pub fn new(max_threads: usize) -> Self {
         assert!(max_threads > 0);
-        Self {
-            size: max_threads,
-            counters: (0..max_threads).map(|_| Default::default()).collect(),
-        }
+        Self { size: max_threads, counters: (0..max_threads).map(|_| Default::default()).collect() }
     }
 
     #[inline]
-    pub fn increment(&self, order: Ordering) {
+    pub fn update(&self, func: impl FnOnce(usize) -> usize) {
+        let idx = self.index();
+        let curr = self.counters[idx].0.load(Ordering::Relaxed);
+        self.counters[idx].0.store(func(curr), Ordering::Relaxed);
+    }
+
+    #[inline]
+    fn index(&self) -> usize {
         let idx = THREAD_ID.with(|id| id.get());
-        assert!(
-            idx < self.size,
-            "more threads than the specified `max_threads` attempted to access the ThreadCounter"
-        );
-
-        self.counters[idx].0.fetch_add(1, order);
-    }
-
-    #[inline]
-    pub fn sum(self) -> usize {
-        self.counters
-            .iter()
-            .map(|count| count.0.load(Ordering::Relaxed))
-            .sum()
+        assert!(idx < self.size, "`max_threads` exceeded");
+        idx
     }
 }
+
+/********** impl IntoIter *************************************************************************/
+
+impl IntoIterator for ThreadCounter {
+    type Item = usize;
+    type IntoIter = IntoIter;
+
+    #[inline]
+    fn into_iter(self) -> Self::IntoIter {
+        IntoIter { idx: 0, counter: self }
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// IntoIter
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+pub struct IntoIter {
+    idx: usize,
+    counter: ThreadCounter,
+}
+
+/********** impl Iterator *************************************************************************/
+
+impl Iterator for IntoIter {
+    type Item = usize;
+
+    #[inline]
+    fn next(&mut self) -> Option<Self::Item> {
+        let idx = self.idx;
+        if idx == self.counter.size {
+            None
+        } else {
+            self.idx += 1;
+            Some(self.counter.counters[idx].0.load(Ordering::Relaxed))
+        }
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// Count
+////////////////////////////////////////////////////////////////////////////////////////////////////
 
 #[repr(align(128))]
 #[derive(Default)]
 struct Count(AtomicUsize);
+
+#[cfg(test)]
+mod tests {
+    use super::ThreadCounter;
+
+    #[test]
+    fn sum() {
+        let counter = ThreadCounter::new(8);
+        let sum: usize = counter.into_iter().sum();
+        assert_eq!(sum, 0usize);
+    }
+}
